b61be4f46a0a20829867a7d94548e6d11ccdeced
Pavel Khramov <paul.khramov@gmail.com>
2022-08-12 21:52:29 +0500

    feature: webapp folder added

--- webapp/weather.py
+++ webapp/weather.py
@@ -1,10 +1,11 @@
 import requests
+from flask import current_app
 
 
 def weather_by_city(city_name):
-    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    weather_url = current_app.config['WEATHER_URL']
     params = {
-        'key': '2bb4bb1aa2c242b9b5a171737221008',
+        'key': current_app.config['WEATHER_API_KEY'],
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,
