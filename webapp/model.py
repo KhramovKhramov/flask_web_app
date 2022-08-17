47c897147c562a4543f182819104cbad4c78ffa1
Pavel Khramov <paul.khramov@gmail.com>
2022-08-17 11:04:19 +0500

    feature: User_db.model added

--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
@@ -12,3 +14,13 @@ class News(db.Model):
 
     def __repr__(self) -> str:
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    password = db.Column(db.String)
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self) -> str:
+        return '<User {}>'.format(self.username)
