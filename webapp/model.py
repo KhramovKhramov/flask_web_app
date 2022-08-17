87ed3ac31bab863efe3782c7078d1f26f2659558
Pavel Khramov <paul.khramov@gmail.com>
2022-08-17 13:47:09 +0500

    feature: log in and user_admin added

--- webapp/model.py
+++ webapp/model.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -21,6 +21,16 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), index=True, unique=True)
     password = db.Column(db.String)
     role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
 
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
