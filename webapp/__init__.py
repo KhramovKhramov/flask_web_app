af01fc4c1002d467418c40cea5cec02f6df7a927
Pavel Khramov <paul.khramov@gmail.com>
2022-08-20 22:40:31 +0500

    feature: migrations added

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.db import db
+from webapp.news.views import blueprint as news_blueprint
 from webapp.user.models import User
 from webapp.user.views import blueprint as user_blueprint
-from webapp.news.views import blueprint as news_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
