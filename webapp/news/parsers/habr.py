509f57931c8d762cbde1bc076d242ade7b503503
Pavel Khramov <paul.khramov@gmail.com>
2022-08-25 09:46:52 +0500

    feature: text_news from habr parsing added

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -1,7 +1,29 @@
-from datetime import datetime
+import locale
+import platform
+from datetime import datetime, timedelta
 
 from bs4 import BeautifulSoup
+from webapp.db import db
+from webapp.news.models import News
 from webapp.news.parsers.utils import get_html, save_news
+
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, "russian")
+else:
+    locale.setlocale(locale.LC_TIME, "ru_RU")
+
+
+def parse_habr_date(date_str):
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    elif 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
 
 
 def get_habr_snippets():
@@ -13,11 +35,18 @@ def get_habr_snippets():
             title = news.find('a', class_='tm-article-snippet__title-link').text
             url = f'https://habr.com{news.find("a", class_="tm-article-snippet__title-link")["href"]}'
             published = news.find('span', class_='tm-article-snippet__datetime-published').text
-            print(title, url, published)
-            """   
-            try:
-                published = datetime.strptime(published, '%Y-%m-%d')
-            except (ValueError):
-                published = datetime.now()
+            published = parse_habr_date(published)
             save_news(title, url, published)
-            """
+
+
+def get_habr_content():
+    news_without_text = News.query.filter(News.text.is_(None))
+    for news in news_without_text:
+        html = get_html(news.url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            news_text = soup.find('div', class_='tm-article-body').decode_contents()
+            if news_text:
+                news.text = news_text
+                db.session.add(news)
+                db.session.commit()
